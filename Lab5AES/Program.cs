c0826b8b72faaa1c164ff7ee066a2e5dd7a71bad
user <user@WIN-T9NTGAQGALP>
2023-12-27 12:57:45 +0200

    Добавьте файлы проекта.

---
+++ Lab5AES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Lab5AES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AES aes = new AES(Encoding.UTF8.GetBytes("1234password1234"));
+
+            byte[] text = Encoding.UTF8.GetBytes("Text 123 Text 12");
+            if (aes.Encrypt(text, ref text))
+            {
+                Console.WriteLine("Зашифрований текст = " + Encoding.UTF8.GetString(text));
+
+                aes.Decrypt(text, ref text);
+                Console.WriteLine("Розшифрований текст = " + Encoding.UTF8.GetString(text));
+            }
+            else
+                Console.WriteLine("Невірні передані дані!");
+        }
+    }
+}
